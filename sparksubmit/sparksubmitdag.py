c4442b15371a2fd25261f4a3061ea8e8442b40b1
f.chavez <f.chavez@ptytechnologies.com>
2024-03-04 03:41:26 -0500

    add send task

--- sparksubmit/sparksubmitdag.py
+++ sparksubmit/sparksubmitdag.py
@@ -8,6 +8,7 @@ with DAG('spark_submit_job',
     submit_job = SparkSubmitOperator(
         task_id='submit_job',
         application='./dags/repo/sparksubmit/pyspark_script.py',
+        files='./dags/repo/sparksubmit/myfile.csv',
         conn_id='spark_default',
         total_executor_cores='1',
         executor_memory='2g',
