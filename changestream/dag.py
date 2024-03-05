4c76e62326be1df13d613059b72ade84eb37b630
f.chavez <f.chavez@ptytechnologies.com>
2024-03-05 12:13:51 -0500

    test changestream

---
+++ changestream/dag.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
+
+args = {
+    'owner': 'fabri'
+}
+
+with DAG(
+
+) as dag:
+    submit_job = SparkSubmitOperator(
+        task_id='submit_job',
+        application='./dags/repo/changestream/mongo_job.py',
+        conn_id='spark_default',
+        total_executor_cores='1',
+        executor_memory='2g',
+        num_executors='1',
+        driver_memory='2g',
+        verbose=False
+    )
+
+submit_job
---
+++ changestream/mongo_job.py
@@ -0,0 +1,28 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.\
+    appName("TrackingStream").\
+    config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12::10.2.1').\
+    getOrCreate()
+
+# define a streaming query
+dataStreamWriter = (spark.readStream
+                    .format("mongodb")
+                    .option("spark.mongodb.connection.uri", "mongodb://root:root@localhost:27017")
+                    .option('spark.mongodb.database', "Bocato")
+                    .option('spark.mongodb.collection', "NotificationTracking")
+                    .option("change.stream.publish.full.document.only", "True")
+                    # .schema(readSchema)
+                    .load()
+                    # manipulate your streaming data
+                    .writeStream
+                    .format("console")
+                    .trigger(continuous="1 second")
+                    .outputMode("append")
+                    )
+
+# run the query
+query = dataStreamWriter.start()
