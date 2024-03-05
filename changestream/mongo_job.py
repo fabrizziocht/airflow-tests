a0cbc3f25df1cc1756533442e38dbd0904b5aae2
f.chavez <f.chavez@ptytechnologies.com>
2024-03-05 13:36:57 -0500

    test changestream

--- changestream/mongo_job.py
+++ changestream/mongo_job.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.sql.types import StructType, StringType
+from pyspark.sql.types import *
 spark = SparkSession.builder.appName("TrackingStream").getOrCreate()
 
 
