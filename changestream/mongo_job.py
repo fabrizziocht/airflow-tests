0785a76304fe8da842bb9d4a5e9f7a048fe91784
f.chavez <f.chavez@ptytechnologies.com>
2024-03-05 13:10:45 -0500

    test changestream

--- changestream/mongo_job.py
+++ changestream/mongo_job.py
@@ -3,17 +3,15 @@ from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.\
-    appName("TrackingStream").\
-    getOrCreate()
-# config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12::10.2.1').\
-# define a streaming query
+spark = SparkSession.builder.appName("TrackingStream").getOrCreate()
+
 dataStreamWriter = (spark.readStream
                     .format("mongodb")
                     .option("spark.mongodb.connection.uri", "mongodb://root:root@mongodb.mongodb.svc.cluster.local:27017")
                     .option('spark.mongodb.database', "Bocato")
                     .option('spark.mongodb.collection', "NotificationTracking")
-                    .option("change.stream.publish.full.document.only", "True")
+                    .option('spark.mongodb.read.readPreference.name', "primaryPreferred")
+                    .option("spark.mongodb.change.stream.publish.full.document.only", "true")
                     # .schema(readSchema)
                     .load()
                     # manipulate your streaming data
