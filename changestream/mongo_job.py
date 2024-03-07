7cc4696eb14f6fcdd9e2fa86e0167b4a15bcd752
f.chavez <f.chavez@ptytechnologies.com>
2024-03-07 03:55:09 -0500

    test

--- changestream/mongo_job.py
+++ changestream/mongo_job.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-spark = SparkSession.builder.appName("TrackingStream").getOrCreate()
+spark = SparkSession.builder.appName("TrackingStream2").getOrCreate()
 
 dataStreamWriter = (spark.readStream
                     .format("mongodb")
@@ -10,13 +10,14 @@ dataStreamWriter = (spark.readStream
                     .option('spark.mongodb.collection', "NotificationTracking")
                     .option('spark.mongodb.read.readPreference.name', "primaryPreferred")
                     .option("spark.mongodb.change.stream.publish.full.document.only", "true")
+                    .option("change.stream.publish.full.document.only", "true")
                     # .schema(readSchema)
                     .load()
                     # manipulate your streaming data
                     .writeStream
                     .format("console")
                     .trigger(continuous="5 second")
-                    .outputMode("update")
+                    .outputMode("append")
                     )
 
 # run the query
