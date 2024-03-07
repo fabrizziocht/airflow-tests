ac23608d1a0104b77c5e41040ac4e13952d6e387
f.chavez <f.chavez@ptytechnologies.com>
2024-03-07 07:50:40 -0500

    test

--- changestream/mongo_job.py
+++ changestream/mongo_job.py
@@ -1,7 +1,58 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-spark = SparkSession.builder.appName("TrackingStream5").getOrCreate()
+spark = SparkSession.builder.appName("TrackingStream6").getOrCreate()
+
+
+readSchema = StructType([
+    StructField("_id", StructType([
+        StructField("$oid", StringType(), True)
+    ]), True),
+    StructField("specversion", StringType(), True),
+    StructField("id", StringType(), True),
+    StructField("source", StringType(), True),
+    StructField("type", StringType(), True),
+    StructField("datacontenttype", StringType(), True),
+    StructField("time", TimestampType(), True),
+    StructField("triggerStatus", StringType(), True),
+    StructField("data", StructType([
+        StructField("eventDetails", StructType([
+            StructField("eventType", StringType(), True),
+            StructField("levelId", IntegerType(), True)
+        ]), True),
+        StructField("transactionDate", TimestampType(), True),
+        StructField("customerId", StringType(), True),
+        StructField("expirationDate", TimestampType(), True),
+        StructField("bonusAmount", StructType([
+            StructField("value", DoubleType(), True),
+            StructField("currency", StringType(), True)
+        ]), True),
+        StructField("requiredRollover", StructType([
+            StructField("value", DoubleType(), True),
+            StructField("currency", StringType(), True)
+        ]), True),
+        StructField("campaignDetails", StructType([
+            StructField("campaignId", IntegerType(), True),
+            StructField("campaignPublicName", StringType(), True)
+        ]), True),
+        StructField("bonusDetails", StructType([
+            StructField("bonusId", IntegerType(), True),
+            StructField("bonusPublicName", StringType(), True),
+            StructField("bonusCategory", StringType(), True),
+            StructField("settings", StructType([
+                StructField("productSettings", StructType([
+                    StructField("sports", StructType([
+                        StructField("allowed", StringType(), True)
+                    ]), True)
+                ]), True)
+            ]), True)
+        ]), True),
+        StructField("depositDetails", StructType([
+            StructField("paymentReference", StringType(), True)
+        ]), True),
+        StructField("tenantId", IntegerType(), True)
+    ]), True)
+])
 
 dataStreamWriter = (spark.readStream
                     .format("mongodb")
@@ -11,7 +62,7 @@ dataStreamWriter = (spark.readStream
                     .option('spark.mongodb.read.readPreference.name', "primaryPreferred")
                     .option("spark.mongodb.change.stream.publish.full.document.only", "true")
                     .option("change.stream.publish.full.document.only", "true")
-                    # .schema(readSchema)
+                    .schema(readSchema)
                     .load()
                     # manipulate your streaming data
                     .writeStream
